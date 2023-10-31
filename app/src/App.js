bdc5d3986c4026c48a89bbb56ee54202ffb5b864
srinath1435 <perumallasrinath2003@gmail.com>
2023-10-31 07:18:46 +0530

    tst

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Go from './components/app'
+
+function App() {
+  return (
+    <div>
+      <Go/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/app.jsx
@@ -0,0 +1,7 @@
+import react from 'react'
+const app =()=>{
+    return(
+        <div>Hello</div>
+    )
+}
+export default app
